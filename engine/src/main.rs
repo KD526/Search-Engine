2eb32ef94a7a386540ec2ed207e374a59701ab35
Kd526 <davisthoyah@gmail.com>
2024-08-17 23:09:31 +0300

    imp: tokenizer splitter

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,25 +1,45 @@
+use std::io;
+use std::fs;
 use std::fs::File;
 use std::process::exit;
+use std::path::Path;
+use std::collections::HashMap;
 use xml::reader::{XmlEvent, EventReader};
 
-fn main() {
-    let file_path = "docs.,gl/gl4/glClear.xhtml";
-    let file = File::open("docs.,gl/gl4/glClear.xhtml").unwrap_or_else(|err| {
-        eprintln!("ERROR: Could not read file {file_path}: {err}");
-        exit(1)
-    });
+
+fn index_doc(_doc_content: &str) -> HashMap<String, usize> {
+    todo!("not implemented");
+
+}
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
     let er = EventReader::new(file);
 
     let mut content = String::new();
 
     for event in er.into_iter() {
-        let event = event.unwrap_or_else(|err| {
-            eprintln!("ERROR: Could not read file {file_path}: {err}");
-            exit(1) 
-        });
-        if let XmlEvent::Characters(text) = event {
+        if let XmlEvent::Characters(text) = event.expect("") {
             content.push_str(&text);
         }
     }
-    println!("{content}");
+    Ok(content)
+
 }
+
+fn main() -> io::Result<()> {
+    //map to a path to a file and the frequency of terms within a file
+    let all_docs = HashMap::<Path, HashMap<String, usize>>::new();    
+    let dir_path = "docs.,gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(&file_path)?;
+        println!("{file_path:?} => {size}", size = &content.len());
+    }
+    
+   //println!("{content}"), content = read_entire_xml_file(file_path).expect("");
+   Ok(())
+}
+
+// To do: implement a tokenizer
