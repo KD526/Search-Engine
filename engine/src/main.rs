ee5ea859ec2948f8dc4ebe4a7410e76bd0a0ff98
Kd526 <davisthoyah@gmail.com>
2025-04-19 23:41:52 +0300

    updates

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -15,7 +15,7 @@ impl<'a> Lexer<'a> {
     fn new(content: &'a[char]) -> Self {
         Self {content}
     }
-
+//THIS  trim checks content and removes spaces
     fn trim_left(&mut self) {
         while.self.content.len() > 0 && self.content[0].is_whitespace() {
             self.content = &self.content[1..];
